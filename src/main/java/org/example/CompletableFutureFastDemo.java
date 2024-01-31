4d8f4e71b14a244017060acf854335d25a72f8d3
Jason <ya850805@gmail.com>
2024-01-31 16:07:35 +0800

    feat: Add CompletableFuture speed compare demo.

---
+++ src/main/java/org/example/CompletableFutureFastDemo.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author jason
+ * @description
+ * @create 2024/1/31 15:57
+ **/
+public class CompletableFutureFastDemo {
+    public static void main(String[] args) {
+        CompletableFuture<String> playA = CompletableFuture.supplyAsync(() -> {
+            System.out.println("A come in");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "playA";
+        });
+
+        CompletableFuture<String> playB = CompletableFuture.supplyAsync(() -> {
+            System.out.println("B come in");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "playB";
+        });
+
+        /**
+         * applyToEither()查看哪個任務比較快
+         */
+        CompletableFuture<String> result = playA.applyToEither(playB, f -> {
+            return f + " is winner";
+        });
+
+        System.out.println(Thread.currentThread().getName() + "\t ----"  + result.join());
+    }
+}
