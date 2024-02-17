6a5fb0b77048b3220cc782b6b1b7166ba97e35c8
Jason <ya850805@gmail.com>
2024-02-17 21:35:40 +0800

    feat: Add atomicReference demo.

---
+++ src/main/java/org/example/cas/AtomicReferenceDemo.java
@@ -0,0 +1,35 @@
+package org.example.cas;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author jason
+ * @description
+ * @create 2024/2/17 21:32
+ **/
+public class AtomicReferenceDemo {
+    public static void main(String[] args) {
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+
+        User z3 = new User("z3", 22);
+        User li4 = new User("li4", 28);
+
+        //初始值
+        atomicReference.set(z3);
+
+        System.out.println(atomicReference.compareAndSet(z3, li4) + "\t" + atomicReference.get().toString());
+        System.out.println(atomicReference.compareAndSet(z3, li4) + "\t" + atomicReference.get().toString());
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class User {
+    private String userName;
+    private Integer age;
+}
