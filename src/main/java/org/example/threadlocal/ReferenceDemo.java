95086015814641bd7eef9749eceacb1e473383ea
Jason <ya850805@gmail.com>
2024-02-25 10:44:39 +0800

    feat: Add soft reference demo.

--- src/main/java/org/example/threadlocal/ReferenceDemo.java
+++ src/main/java/org/example/threadlocal/ReferenceDemo.java
@@ -1,5 +1,8 @@
 package org.example.threadlocal;
 
+import java.lang.ref.SoftReference;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author jason
  * @description
@@ -7,6 +10,31 @@ package org.example.threadlocal;
  **/
 public class ReferenceDemo {
     public static void main(String[] args) {
+//        strongReference();
+
+        SoftReference<MyObject> softReference = new SoftReference<>(new MyObject());
+        System.out.println("---- softReference: " + softReference.get());
+
+        //呼叫垃圾回收並等待他執行
+        System.gc();
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println("---- after gc內存夠用：" + softReference.get());
+
+        //這邊設置runtime虛擬機參數設置內存
+        /**
+         * -Xms10m -Xmx10m
+         */
+
+        try {
+            byte[] bytes = new byte[20 * 1024 * 1024]; //20M 對象
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("---- after gc內存不夠：" + softReference.get());
+        }
+    }
+
+    private static void strongReference() {
         MyObject myObject = new MyObject();
         System.out.println("before gc: " + myObject);
 
